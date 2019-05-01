3151e1046ac0fdc66bd17df02db7c9c067723312
Tamas Majoros <tmsmajoros@gmail.com>
2019-05-01 20:18:38 +0200

    added sitemap generator

--- app.js
+++ app.js
@@ -26,26 +26,26 @@ mongoose.set('useCreateIndex', true);
 app.set('view engine', 'ejs');
 app.use(flash());
 
-// SITEMAP GENERATOR (ADD sitemap.xml to google console)
-// var sitemap = sm.createSitemap({
-//   hostname: 'https://nogradapro.com',
-//   cacheTime: 600000, // 600 sec - cache purge period
-//   urls: [{
-//     url: '/ads/',
-//     changefreq: 'daily',
-//     priority: 0.3
-//   }]
-// });
+SITEMAP GENERATOR (ADD sitemap.xml to google console)
+var sitemap = sm.createSitemap({
+  hostname: 'https://smartbee.info',
+  cacheTime: 600000, // 600 sec - cache purge period
+  urls: [{
+    url: '/ads/',
+    changefreq: 'daily',
+    priority: 0.3
+  }]
+});
 
-// app.get('/sitemap.xml', function (req, res) {
-//   sitemap.toXML(function (err, xml) {
-//     if (err) {
-//       return res.status(500).end();
-//     }
-//     res.header('Content-Type', 'application/xml');
-//     res.send(xml);
-//   });
-// });
+app.get('/sitemap.xml', function (req, res) {
+  sitemap.toXML(function (err, xml) {
+    if (err) {
+      return res.status(500).end();
+    }
+    res.header('Content-Type', 'application/xml');
+    res.send(xml);
+  });
+});
 
 // PASSPORT CONFIG
 app.use(require('express-session')({
