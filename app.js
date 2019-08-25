19623166a0c8dc3c7a0b4d75d6c9450e6c3dc6f9
Tamas Majoros <tmsmajoros@gmail.com>
2019-08-25 19:09:41 +0200

    Added -dev utilities (ESlint, prettier, dotenv)

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const methodOverride = require('method-override');
-const expressSanitizer = require("express-sanitizer");
+const expressSanitizer = require('express-sanitizer');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const flash = require('connect-flash');
-const middleware = require('./middleware');
 const sm = require('sitemap');
+const dotenv = require('dotenv');
 const sslRedirect = require('heroku-ssl-redirect'); // SSL Redirect, must have heroku
+
 const app = express();
 
+dotenv.config({ path: './.env' });
 const blogsRoutes = require('./routes/blog');
 const indexRoutes = require('./routes/index');
 // const adminRoutes = require('./routes/admin');
@@ -18,28 +20,38 @@ const User = require('./models/user');
 
 app.locals.moment = require('moment');
 
-mongoose.connect('mongodb://tmajoros:Tmsmajoros1977@ds147096.mlab.com:47096/smpl-blog', {
-  useNewUrlParser: true
-});
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
+// Use variable to store database url and password, from .env
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
+// Connect to Database
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log(` \x1b[34mMongoDB connection successful!\x1b[0m\n`));
 app.set('view engine', 'ejs');
 app.use(flash());
 
 //SITEMAP GENERATOR (ADD sitemap.xml to google console)
-var sitemap = sm.createSitemap({
+const sitemap = sm.createSitemap({
   hostname: 'https://smartbee.info',
   cacheTime: 600000, // 600 sec - cache purge period
-  urls: [{
-    url: '/blogs/',
-    changefreq: 'daily',
-    priority: 0.3
-  }]
+  urls: [
+    {
+      url: '/blogs/',
+      changefreq: 'daily',
+      priority: 0.3
+    }
+  ]
 });
 
-app.get('/sitemap.xml', function (req, res) {
-  sitemap.toXML(function (err, xml) {
+app.get('/sitemap.xml', function(req, res) {
+  sitemap.toXML(function(err, xml) {
     if (err) {
       return res.status(500).end();
     }
@@ -49,30 +61,34 @@ app.get('/sitemap.xml', function (req, res) {
 });
 
 // PASSPORT CONFIG
-app.use(require('express-session')({
-  secret: 'I know what I want.',
-  resave: false,
-  saveUninitialized: false
-}));
+app.use(
+  require('express-session')({
+    secret: 'I know what I want.',
+    resave: false,
+    saveUninitialized: false
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(express.static(__dirname + '/public'));
-app.use(methodOverride("_method"));
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(express.static(`${__dirname}/public`));
+app.use(methodOverride('_method'));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 app.use(sslRedirect()); // Redirect Heroku SSl. MUST HAVE to HEROKU!!
 app.use(expressSanitizer());
 
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   res.locals.currentUser = req.user;
   res.locals.error = req.flash('error');
   res.locals.success = req.flash('success');
-  res.locals.pageUrl = 'https://' + req.get('host') + req.originalUrl; // SHOW actual page's URL
+  res.locals.pageUrl = `https://${req.get('host')}${req.originalUrl}`; // SHOW actual page's URL
   next();
 });
 
@@ -86,15 +102,14 @@ app.use('/blogs', blogsRoutes);
 // })
 
 // 404 ERROR PAGE
-app.get('*', function (req, res) {
+app.get('*', function(req, res) {
   res.render('404');
 });
 
-
-// Listening PORT, server is running 
-let PORT = process.env.PORT || 3000;
+// Listening PORT, server is running
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('========================================================')
-  console.log(`Maja SMPL BLOG. SERVER STARTED ON PORT ${PORT}`)
-  console.log('========================================================')
+  console.log('========================================================');
+  console.log(`Maja SMPL BLOG. SERVER STARTED ON PORT ${PORT}`);
+  console.log('========================================================');
 });
