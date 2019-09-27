2c94efb2b3b763ee5698437bc797465363b060c0
Tamas Majoros <tmsmajoros@gmail.com>
2019-09-27 12:01:05 +0200

    Delete 'twitter' scripts, minimal design modify (footer)

--- routes/index.js
+++ routes/index.js
@@ -3,34 +3,36 @@ var router = express.Router();
 var passport = require('passport');
 var User = require('../models/user');
 
-router.get('/', function (req, res) {
+router.get('/', function(req, res) {
   res.redirect('/blogs');
 });
-
 
 // AUTHORIZATION
 
 // Login
-router.get('/login', function (req, res) {
+router.get('/login', function(req, res) {
   res.render('login');
 });
 
 // login logic
-router.post('/login', passport.authenticate('local', {
-  successRedirect: '/blogs',
-  failureRedirect: '/login',
-  failureFlash: true,
-  successFlash: 'Sikeres bejelentkezés!'
-}), function (req, res) {});
-
+router.post(
+  '/login',
+  passport.authenticate('local', {
+    successRedirect: '/blogs',
+    failureRedirect: '/login',
+    failureFlash: true,
+    successFlash: 'Sikeres bejelentkezés!'
+  }),
+  function(req, res) {}
+);
 
 // Register
-router.get('/register', function (req, res) {
+router.get('/register', function(req, res) {
   res.render('register');
 });
 
 // handling sign up form
-router.post('/register', function (req, res) {
+router.post('/register', function(req, res) {
   if (req.body.password === req.body.samePassword) {
     var newUser = new User({
       username: req.body.username,
@@ -38,15 +40,15 @@ router.post('/register', function (req, res) {
     });
     if (req.body.username === 'Admin') {
       newUser.isAdmin = true;
-    };
-    User.register(newUser, req.body.password, function (err, regUser) {
+    }
+    User.register(newUser, req.body.password, function(err, regUser) {
       if (err) {
         req.flash('error', err.message);
         console.log(err.message);
         res.redirect('/register');
       } else {
-        passport.authenticate('local')(req, res, function () {
-          req.flash('success', 'Üdvözlet a SMPL Blogon ' + regUser.username + '.');
+        passport.authenticate('local')(req, res, function() {
+          req.flash('success', 'Üdvözlet a Smart Bee Blogon ' + regUser.username + '.');
           res.redirect('/blogs');
         });
       }
@@ -57,7 +59,7 @@ router.post('/register', function (req, res) {
   }
 });
 
-router.get('/logout', function (req, res) {
+router.get('/logout', function(req, res) {
   req.logout();
   req.flash('success', 'Sikeresen kijelentkeztél!');
   res.redirect('/blogs');
